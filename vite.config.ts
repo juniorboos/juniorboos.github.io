5770a51c426a53a84f829da607b60e46e9529c2c
Milton Boos Junior <junior_boos@live.com>
2023-01-12 20:18:32 +0000

    configure path alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+import * as path from "path";
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
@@ -8,7 +10,19 @@ export default defineConfig({
     alias: [
       {
         find: "~stitches",
-        replacement: "./src/theme/stitches.config.ts",
+        replacement: path.resolve(__dirname, "src/theme/stitches.config.ts"),
+      },
+      {
+        find: "~atoms",
+        replacement: path.resolve(__dirname, "./src/components/atoms"),
+      },
+      {
+        find: "~molecules",
+        replacement: path.resolve(__dirname, "./src/components/molecules"),
+      },
+      {
+        find: "~theme",
+        replacement: path.resolve(__dirname, "./src/theme"),
       },
     ],
   },
