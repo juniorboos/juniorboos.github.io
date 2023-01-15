66603b06571ef3da8e87c910a5b7182227d9ecfb
Milton Boos Junior <junior_boos@live.com>
2023-01-15 05:10:37 +0000

    add button hover state

--- src/components/atoms/button.ts
+++ src/components/atoms/button.ts
@@ -5,7 +5,15 @@ const Button = styled("button", {
   border: "1px solid #E964FF",
   borderRadius: "10px",
   color: "#E964FF",
-  padding: "1rem 3rem",
+  padding: "0.75rem 3rem",
+  cursor: "pointer",
+  fontSize: "1rem",
+
+  "&:hover": {
+    backgroundColor: "#E964FF",
+    color: "black",
+    fontWeight: "bold",
+  },
 });
 
 export { Button };
