3958741c45729beedb481e32385481c35c18a7d5
Milton Boos Junior <junior_boos@live.com>
2023-02-08 22:05:29 +0000

    improved technologies

---
+++ src/content/technologies.ts
@@ -0,0 +1,196 @@
+import {
+  SiCss3,
+  SiCypress,
+  SiFigma,
+  SiFirebase,
+  SiGithub,
+  SiGithubactions,
+  SiGitlab,
+  SiGo,
+  SiGooglecloud,
+  SiJavascript,
+  SiJest,
+  SiKeras,
+  SiMaterialui,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiPython,
+  SiReact,
+  SiRedux,
+  SiSqlite,
+  SiStorybook,
+  SiStyledcomponents,
+  SiTensorflow,
+  SiTypescript,
+} from "react-icons/si";
+
+const python = {
+  Icon: SiPython,
+  name: "Python",
+  url: "https://www.python.org",
+};
+
+const tensorflow = {
+  Icon: SiTensorflow,
+  name: "TensorFlow",
+  url: "https://www.tensorflow.org",
+};
+
+const keras = {
+  Icon: SiKeras,
+  name: "Keras",
+  url: "https://keras.io",
+};
+
+const sqlite = {
+  Icon: SiSqlite,
+  name: "SQLite",
+  url: "https://www.sqlite.org",
+};
+
+const react = {
+  Icon: SiReact,
+  name: "React",
+  url: "https://reactjs.org",
+};
+
+const reactNative = {
+  Icon: SiReact,
+  name: "React Native",
+  url: "https://reactnative.dev",
+};
+
+const firebase = {
+  Icon: SiFirebase,
+  name: "Firebase",
+  url: "https://firebase.google.com",
+};
+
+const styledComponents = {
+  Icon: SiStyledcomponents,
+  name: "Styled Components",
+  url: "https://styled-components.com",
+};
+
+const figma = {
+  Icon: SiFigma,
+  name: "Figma",
+  url: "https://www.figma.com",
+};
+
+const googleCloud = {
+  Icon: SiGooglecloud,
+  name: "Google Cloud Platform",
+  url: "https://cloud.google.com/",
+};
+
+const css3 = {
+  Icon: SiCss3,
+  name: "CSS",
+  url: "https://developer.mozilla.org/en-US/docs/Web/CSS",
+};
+
+const redux = {
+  Icon: SiRedux,
+  name: "Redux",
+  url: "https://redux.js.org",
+};
+
+const typescript = {
+  Icon: SiTypescript,
+  name: "Typescript",
+  url: "https://www.typescriptlang.org",
+};
+
+const javascript = {
+  Icon: SiJavascript,
+  name: "Javascript",
+  url: "https://developer.mozilla.org/pt-BR/docs/Web/JavaScript",
+};
+
+const storybook = {
+  Icon: SiStorybook,
+  name: "Storybook",
+  url: "https://storybook.js.org",
+};
+
+const jest = {
+  Icon: SiJest,
+  name: "Jest",
+  url: "https://jestjs.io",
+};
+
+const cypress = {
+  Icon: SiCypress,
+  name: "Cypress",
+  url: "https://www.cypress.io",
+};
+
+const nextjs = {
+  Icon: SiNextdotjs,
+  name: "Next.js",
+  url: "https://nextjs.org",
+};
+
+const gitlab = {
+  Icon: SiGitlab,
+  name: "GitLab",
+  url: "https://about.gitlab.com",
+};
+
+const github = {
+  Icon: SiGithub,
+  name: "GitHub",
+  url: "https://github.com/",
+};
+
+const githubActions = {
+  Icon: SiGithubactions,
+  name: "GitHub Actions",
+  url: "https://github.com/features/actions",
+};
+
+const golang = {
+  Icon: SiGo,
+  name: "Golang",
+  url: "https://go.dev",
+};
+
+const materialui = {
+  Icon: SiMaterialui,
+  name: "Material UI",
+  url: "https://mui.com",
+};
+
+const nodejs = {
+  Icon: SiNodedotjs,
+  name: "Node.js",
+  url: "https://nodejs.org/en/",
+};
+
+export {
+  python,
+  tensorflow,
+  keras,
+  sqlite,
+  react,
+  reactNative,
+  nextjs,
+  redux,
+  figma,
+  firebase,
+  styledComponents,
+  googleCloud,
+  css3,
+  javascript,
+  typescript,
+  storybook,
+  jest,
+  cypress,
+  gitlab,
+  github,
+  githubActions,
+  golang,
+  materialui,
+  nodejs,
+};
