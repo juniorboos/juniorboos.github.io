39ba6d085178abf38d86470c0506c84376bda58d
Milton Boos Junior <junior_boos@live.com>
2024-02-01 14:58:33 +0000

    update about section

--- src/content/about.ts
+++ src/content/about.ts
@@ -4,7 +4,7 @@ export default {
   title: "About me",
   description: [
     "As a software developer, I am driven and passionate about my work in the technology industry. With a Bachelor's degree in Computer Science and a Master's degree in Informatics, I have a strong foundation in the field and a commitment to continuous learning. There, I practiced my abilities in building cloud architecture for IoT environments and gained hands-on experience in both web and mobile development.",
-    "For the past few years, I’ve had the privilege of working at a startup, a consultancy company, an e-commerce and as a freelancer. My expertise is in front-end development and I'm currently focusing on building accessible, inclusive products for xgeeks.",
+    "For the past few years, I’ve had the privilege of working at a startup, a consultancy company, an e-commerce and as a freelancer. My expertise is in front-end development and I'm currently focusing on building accessible, inclusive products for Fignum through Lisbon Nearshore.",
   ],
   profilePicture: ProfilePicture,
 };
