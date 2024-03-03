918dc7d27d832d3ad5c733fd8133e7bd705c6121
Milton Boos Junior <junior_boos@live.com>
2024-03-03 15:58:29 +0000

    Update cv link

--- src/content/intro.ts
+++ src/content/intro.ts
@@ -26,6 +26,6 @@ export default {
   ],
   cv: {
     text: "Download my CV",
-    url: "https://drive.google.com/file/d/1iZOVokY0SrYualPx1PSRVqsz8fXO8FYT/view?usp=sharing",
+    url: "https://drive.google.com/file/d/1HIZleWkBlLphtsnm1SAPibnF7sM_tWH4/view?usp=sharing",
   },
 };
